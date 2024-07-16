5145bff96e89b5257013694939baa7739559223e
Chris Salvador <csalvador58@gmail.com>
2024-07-16 15:05:37 -0700

    Initial Commit

---
+++ MyPantry/MyPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPantryApp.swift
+//  MyPantry
+//
+//  Created by Salvador on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
