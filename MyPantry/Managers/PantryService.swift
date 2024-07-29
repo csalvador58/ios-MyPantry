ad51bf6dcacb0f89260fe0297516e689ae14d4eb
Chris Salvador <csalvador58@gmail.com>
2024-07-29 16:42:38 -0700

    [Update] - Integrated updated CloudKit service

--- MyPantry/Managers/PantryService.swift
+++ MyPantry/Managers/PantryService.swift
@@ -25,36 +25,39 @@ protocol PantryServiceType {
     func addPantry(_ pantry: Pantry, ownerId: String) async throws -> Pantry
     func updatePantry(_ pantry: Pantry) async throws -> Pantry
     func deletePantry(_ pantry: Pantry) async throws
+    func fetchOrCreateShare(for pantry: Pantry) async throws -> (CKShare, CKContainer)
+    func acceptShare(metadata: CKShare.Metadata) async throws
 }
 
 struct PantryService: PantryServiceType {
     let ckDB: CKDatabase
+    let cloudKitService: CloudKitServiceType
 
-    init() {
-        ckDB = CKContainer(identifier: Config.containerIdentifier).privateCloudDatabase
+    init(cloudKitService: CloudKitServiceType = CloudKitService()) {
+        self.cloudKitService = cloudKitService
+        self.ckDB = CKContainer(identifier: Config.containerIdentifier).privateCloudDatabase
     }
 
     func fetchPantry(by ownerId: String) async throws -> [Pantry] {
         let predicate = NSPredicate(format: "ownerId == %@", ownerId)
-        let query = CKQuery(recordType: Pantry.recordType, predicate: predicate)
+        let query = CKQuery(recordType: PantryConverter.recordType, predicate: predicate)
 //        let query = CKQuery(recordType: Pantry.type, predicate: NSPredicate(value: true))
 
         let (matchResults, _) = try await ckDB.records(matching: query)
         let records = matchResults.compactMap { try? $0.1.get() }
 
-        return records.compactMap { Pantry.fromRecord($0) }
+        return records.compactMap { PantryConverter.fromRecord($0) }
     }
 
     func addPantry(_ pantry: Pantry, ownerId: String) async throws -> Pantry {
-        let newPantry = Pantry(
-            id: CKRecord.ID(recordName: UUID().uuidString),
-            name: pantry.name,
-            ownerId: pantry.ownerId
-        )
+        var newPantry = pantry
+        if pantry.isShared {
+            newPantry = try await cloudKitService.createSharedZone(for: pantry)
+        }
 
-        let record = newPantry.toRecord()
+        let record = PantryConverter.toRecord(newPantry)
         let saveRecord = try await ckDB.save(record)
-        guard let savedPantry = Pantry.fromRecord(saveRecord) else {
+        guard let savedPantry = PantryConverter.fromRecord(saveRecord) else {
             throw PantryServiceError.failedToSavePantry
         }
 
@@ -63,16 +66,17 @@ struct PantryService: PantryServiceType {
 
     func updatePantry(_ pantry: Pantry) async throws -> Pantry {
         let recordId = pantry.id
-        let record = try await ckDB.record(for: recordId)
+        let record = try await ckDB.record(for: CKRecord.ID(recordName: recordId))
 
         record[Pantry.CodingKeys.name.rawValue] = pantry.name
         record[Pantry.CodingKeys.ownerId.rawValue] = pantry.ownerId
-        record[Pantry.CodingKeys.shareReference.rawValue] = pantry.shareReference
+        record[Pantry.CodingKeys.shareReferenceId.rawValue] = pantry.shareReferenceId
         record[Pantry.CodingKeys.isShared.rawValue] = pantry.isShared
+        record[Pantry.CodingKeys.zoneId.rawValue] = pantry.zoneId
 
         let updateRecord = try await ckDB.save(record)
 
-        guard let updatedPantry = Pantry.fromRecord(updateRecord) else {
+        guard let updatedPantry = PantryConverter.fromRecord(updateRecord) else {
             throw PantryServiceError.failedToUpdatePantry
         }
 
@@ -81,6 +85,14 @@ struct PantryService: PantryServiceType {
 
     func deletePantry(_ pantry: Pantry) async throws {
         let recordId = pantry.id
-        try await ckDB.deleteRecord(withID: recordId)
+        try await ckDB.deleteRecord(withID: CKRecord.ID(recordName: recordId))
+    }
+    
+    func fetchOrCreateShare(for pantry: Pantry) async throws -> (CKShare, CKContainer) {
+        try await cloudKitService.fetchOrCreateShare(for: pantry)
+    }
+    
+    func acceptShare(metadata: CKShare.Metadata) async throws {
+        try await cloudKitService.acceptShare(metadata: metadata)
     }
 }
